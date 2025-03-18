99428a945e70310710d4a5d0c447323d2ac04fd6
Silvio <silviosamysilva@hotmail.com>
2025-03-18 13:40:30 -0300

    Add files via upload

---
+++ javaCurso/Training/Util.java
@@ -0,0 +1,12 @@
+package Training;
+
+public class Util {
+
+    public CarBrand newBrand(String name) {
+        return new CarBrand(name);
+    }
+
+    public CarModel newModel(String name) {
+        return new CarModel(name);
+    }
+}
