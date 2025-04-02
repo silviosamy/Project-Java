d2860a4a0714e71eb80d4793c18b3176c4c75f7c
Silvio <silviosamysilva@hotmail.com>
2025-04-02 18:50:52 -0300

    Add files via upload

---
+++ carro-pessoa/src/main/java/org/example/Carro.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.List;
+
+public class Carro {
+
+    private String nome;
+    private List<Problema> listProblemas;
+
+    public Carro(String nome) {
+        this.nome = nome;
+        this.listProblemas = listProblemas;
+    }
+
+    public List<Problema> getListProblemas() {
+        return listProblemas;
+    }
+
+    public void setListProblemas(List<Problema> listProblemas) {
+        this.listProblemas = listProblemas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ carro-pessoa/src/main/java/org/example/Problema.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Problema {
+
+    private String problema;
+
+    public String getProblema() {
+        return problema;
+    }
+
+    public void setProblema(String problema) {
+        this.problema = problema;
+    }
+}
